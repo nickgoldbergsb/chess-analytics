bca0503d4491b79474cc20c72086835ac3ef7fba
nicholas.goldberg <nicholas.goldberg@cox.net>
2023-09-04 20:51:30 +0100

    Fixed airflow and made api calls async

--- Airflow/dags/s3_upload.py
+++ Airflow/dags/s3_upload.py
@@ -1,15 +1,45 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.hooks.S3_hook import S3Hook
+from airflow.providers.amazon.aws.operators.s3 import S3CreateObjectOperator
 
-from extract_functions import get_monthly_games, get_blitz_game_statistics, get_rapid_game_statistics
+import asyncio
+import json
+from chessdotcom.aio import get_player_stats, get_player_games_by_month, Client
 from datetime import datetime
 
-def upload_to_s3(filename: str, key: str, bucket_name: str) -> None:
-    hook = S3Hook('s3_conn')
-    hook.load_file(filename=filename, key=key, bucket_name=bucket_name)
+Client.aio = True
 
-with DAG('s3_upload', 
+async def gather_cors(cor):
+    response = await asyncio.gather(cor)
+    return response
+
+currentMonth = datetime.now().month
+currentYear = datetime.now().year
+currentDay = datetime.now().day
+date_string = f"{currentYear}-{currentMonth}-{currentDay}"
+
+def get_monthly_games(username, year=currentYear, month=currentMonth):
+    cor = get_player_games_by_month(username=username,year=year, month=month)
+    response = asyncio.run(cor).json
+    raw_monthly_games = response['games']
+
+    return raw_monthly_games
+
+def get_rapid_game_statistics(username):
+    cor = get_player_stats(username=username)
+    response = asyncio.run(cor).json
+    raw_chess_rapid_statistics = response['stats']['chess_rapid']
+
+    return raw_chess_rapid_statistics
+
+def get_blitz_game_statistics(username):
+    cor = get_player_stats(username=username)
+    response = asyncio.run(cor).json
+    raw_chess_blitz_statistics = response['stats']['chess_blitz']
+
+    return raw_chess_blitz_statistics
+
+with DAG('s3_upload_xcom', 
          start_date=datetime(2022, 1, 1),
          schedule_interval='@monthly', 
          catchup=False) as dag:
@@ -17,6 +47,14 @@ with DAG('s3_upload',
     task_get_monthly_games = PythonOperator(
         task_id='get_monthly_games',
         python_callable=get_monthly_games,
+        op_kwargs={
+            'username':'nickgoldbergg'
+        }
+    )
+
+    task_get_rapid_game_statistics = PythonOperator(
+        task_id='get_rapid_game_statistics',
+        python_callable=get_rapid_game_statistics,
         op_kwargs={
             'username':'nickgoldbergg'
         }
@@ -30,43 +68,28 @@ with DAG('s3_upload',
         }
     )
 
-    task_get_rapid_game_statistics = PythonOperator(
-        task_id='get_blitz_game_statistics',
-        python_callable=get_rapid_game_statistics,
-        op_kwargs={
-            'username':'nickgoldbergg'
-        }
-    )
-    
-    task_upload_monthly_to_s3 = PythonOperator(
-        task_id='upload_to_s3',
-        python_callable=upload_to_s3,
-        op_kwargs={
-            'filename': 'resources/data/raw_monthly_games.json',
-            'key':'test.csv',
-            'bucket_name': 'chess-analytics-nickgoldbergg'
-        }
+    task_upload_monthly_to_s3 = S3CreateObjectOperator(
+        task_id='upload_to_s3_monthly',
+        aws_conn_id='s3_conn',
+        s3_bucket='chess-analytics-nickgoldbergg',
+        s3_key=f'raw/{date_string}/raw_monthly_games.json',
+        data='{{ task_instance.xcom_pull(task_ids="task_get_monthly_games") }}'
     )
 
-    task_upload_rapid_to_s3 = PythonOperator(
-        task_id='upload_to_s3',
-        python_callable=upload_to_s3,
-        op_kwargs={
-            'filename': 'resources/data/raw_chess_rapid_statistics.json',
-            'key':'test.csv',
-            'bucket_name': 'chess-analytics-nickgoldbergg'
-        }
+    task_upload_rapid_to_s3 = S3CreateObjectOperator(
+        task_id='upload_to_s3_rapid',
+        aws_conn_id='s3_conn',
+        s3_bucket='chess-analytics-nickgoldbergg',
+        s3_key=f'raw/{date_string}/raw_chess_rapid_statistics.json',
+        data='{{ task_instance.xcom_pull(task_ids="task_get_rapid_game_statistics") }}'
     )
 
-    task_upload_blitz_to_s3 = PythonOperator(
-        task_id='upload_to_s3',
-        python_callable=upload_to_s3,
-        op_kwargs={
-            'filename': 'resources/data/raw_chess_blitz_statistics.json',
-            'key':'test.csv',
-            'bucket_name': 'chess-analytics-nickgoldbergg'
-        }
+    task_upload_blitz_to_s3 = S3CreateObjectOperator(
+        task_id='upload_to_s3_blitz',
+        aws_conn_id='s3_conn',
+        s3_bucket='chess-analytics-nickgoldbergg',
+        s3_key=f'raw/{date_string}/raw_chess_blitz_statistics.json',
+        data='{{ task_instance.xcom_pull(task_ids="task_get_blitz_game_statistics") }}'
     )
 
-    task_get_monthly_games >> task_get_blitz_game_statistics >> task_get_rapid_game_statistics >> task_upload_monthly_to_s3 >> task_upload_rapid_to_s3 >> task_upload_blitz_to_s3
-
+    task_get_monthly_games >> task_get_rapid_game_statistics >> task_get_blitz_game_statistics >> task_upload_monthly_to_s3 >> task_upload_rapid_to_s3 >> task_upload_blitz_to_s3
